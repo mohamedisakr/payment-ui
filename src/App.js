38ee7e3a13f2307328aa11071718c3a38ea22d9f
mohamedisakr <mohamedisakr@gmail.com>
2022-03-01 13:40:30 +0200

    adding credit card basic layout

--- src/App.js
+++ src/App.js
@@ -101,15 +101,15 @@ function App() {
 
         <div className="input-container mt">
           <h4>Enter card number</h4>
-          {/* <Cleave
-            delimiter="-"
-            options={{
-              creditCard: true,
-              onCreditCardTypeChanged: handleType,
-            }}
-            onChange={handleNum}
-            placeholder="Please enter your credit card number"
-          /> */}
+          {/* //  <Cleave
+          //   delimiter="-"
+          //   options={{
+          //     creditCard: true,
+          //     onCreditCardTypeChanged: handleType,
+          //   }}
+          //   onChange={handleNum}
+          //   placeholder="Please enter your credit card number"
+          // />  */}
         </div>
 
         <div className="input-container">
@@ -124,6 +124,10 @@ function App() {
 
         <div className="input-grp">
           <div className="input-container">
+            <h4>Expiration Date</h4>
+            <input type="text" placeholder="MM/YYYY" required />
+          </div>
+          {/* <div className="input-container">
             <h4>Expiration Year</h4>
             <select value={expireYear} onChange={handleExpYear}>
               <option value="January">January</option>
@@ -139,8 +143,9 @@ function App() {
               <option value="November">November</option>
               <option value="December">December</option>
             </select>
-          </div>
-          <div className="input-container">
+          </div> */}
+
+          {/* <div className="input-container">
             <h4>Month</h4>
             <select value={expireMonth} onChange={handleExpMonth}>
               <option value="2021">2021</option>
@@ -153,10 +158,14 @@ function App() {
               <option value="2028">2028</option>
               <option value="2029">2029</option>
             </select>
-          </div>
+          </div> */}
           <div className="input-container">
             <h4>CVV</h4>
-            <input type="password" placeholder="CVV" required />
+            <input type="text" placeholder="CVV" required />
+          </div>
+          <div className="input-container">
+            <h4>Amount</h4>
+            <input type="text" placeholder="Amount" required />
           </div>
         </div>
 
@@ -189,3 +198,227 @@ App.propTypes = {
   preview: PropTypes.bool,
 }
 export default App
+
+/*
+import React from 'react'
+import PropTypes from 'prop-types'
+import Payment from 'payment'
+
+const App = (props) => {
+  const issuer = () => {
+    const {issuer, preview} = props
+
+    return preview && issuer ? issuer.toLowerCase() : this.options.issuer
+  }
+
+  const number = () => {
+    const {number, preview} = props
+
+    let maxLength = preview ? 19 : this.options.maxLength
+    let nextNumber =
+      typeof number === 'number'
+        ? number.toString()
+        : number.replace(/[A-Za-z]| /g, '')
+
+    if (isNaN(parseInt(nextNumber, 10)) && !preview) {
+      nextNumber = ''
+    }
+
+    if (maxLength > 16) {
+      maxLength = nextNumber.length <= 16 ? 16 : maxLength
+    }
+
+    if (nextNumber.length > maxLength) {
+      nextNumber = nextNumber.slice(0, maxLength)
+    }
+
+    while (nextNumber.length < maxLength) {
+      nextNumber += '•'
+    }
+
+    if (['amex', 'dinersclub'].includes(this.issuer)) {
+      const format = [0, 4, 10]
+      const limit = [4, 6, 5]
+      nextNumber = `${nextNumber.substr(
+        format[0],
+        limit[0],
+      )} ${nextNumber.substr(format[1], limit[1])} ${nextNumber.substr(
+        format[2],
+        limit[2],
+      )}`
+    } else if (nextNumber.length > 16) {
+      const format = [0, 4, 8, 12]
+      const limit = [4, 7]
+      nextNumber = `${nextNumber.substr(
+        format[0],
+        limit[0],
+      )} ${nextNumber.substr(format[1], limit[0])} ${nextNumber.substr(
+        format[2],
+        limit[0],
+      )} ${nextNumber.substr(format[3], limit[1])}`
+    } else {
+      for (let i = 1; i < maxLength / 4; i++) {
+        const space_index = i * 4 + (i - 1)
+        nextNumber = `${nextNumber.slice(0, space_index)} ${nextNumber.slice(
+          space_index,
+        )}`
+      }
+    }
+
+    return nextNumber
+  }
+
+  const expiry = () => {
+    const {expiry = ''} = props
+    const date = typeof expiry === 'number' ? expiry.toString() : expiry
+    let month = ''
+    let year = ''
+
+    if (date.includes('/')) {
+      ;[month, year] = date.split('/')
+    } else if (date.length) {
+      month = date.substr(0, 2)
+      year = date.substr(2, 6)
+    }
+
+    while (month.length < 2) {
+      month += '•'
+    }
+
+    if (year.length > 2) {
+      year = year.substr(2, 4)
+    }
+
+    while (year.length < 2) {
+      year += '•'
+    }
+
+    return `${month}/${year}`
+  }
+
+  const options = () => {
+    const {number} = props
+    const issuer = Payment.fns.cardType(number) || 'unknown'
+
+    let maxLength = 16
+
+    if (issuer === 'amex') {
+      maxLength = 15
+    } else if (issuer === 'dinersclub') {
+      maxLength = 14
+    } else if (['hipercard', 'mastercard', 'visa'].includes(issuer)) {
+      maxLength = 19
+    }
+
+    return {
+      issuer,
+      maxLength,
+    }
+  }
+
+  const setCards = () => {
+    const {acceptedCards} = props
+    let newCardArray = []
+
+    if (acceptedCards.length) {
+      Payment.getCardArray().forEach((d) => {
+        if (acceptedCards.includes(d.type)) {
+          newCardArray.push(d)
+        }
+      })
+    } else {
+      newCardArray = newCardArray.concat(Payment.getCardArray())
+    }
+
+    Payment.setCardArray(newCardArray)
+  }
+
+  const {cvc, focused, locale, name, placeholders} = props
+  // const {number, expiry} = this
+
+  return (
+    <div key="Cards" className="rccs">
+      <div
+        className={[
+          'rccs__card',
+          `rccs__card--${this.issuer}`,
+          focused === 'cvc' && this.issuer !== 'amex'
+            ? 'rccs__card--flipped'
+            : '',
+        ]
+          .join(' ')
+          .trim()}
+      >
+        <div className="rccs__card--front">
+          <div className="rccs__card__background" />
+          <div className="rccs__issuer" />
+          <div
+            className={[
+              'rccs__cvc__front',
+              focused === 'cvc' ? 'rccs--focused' : '',
+            ]
+              .join(' ')
+              .trim()}
+          >
+            {cvc}
+          </div>
+          <div
+            className={[
+              'rccs__number',
+              number.replace(/ /g, '').length > 16 ? 'rccs__number--large' : '',
+              focused === 'number' ? 'rccs--focused' : '',
+              number.substr(0, 1) !== '•' ? 'rccs--filled' : '',
+            ]
+              .join(' ')
+              .trim()}
+          >
+            {number}
+          </div>
+          <div
+            className={[
+              'rccs__name',
+              focused === 'name' ? 'rccs--focused' : '',
+              name ? 'rccs--filled' : '',
+            ]
+              .join(' ')
+              .trim()}
+          >
+            {name || placeholders.name}
+          </div>
+          <div
+            className={[
+              'rccs__expiry',
+              focused === 'expiry' ? 'rccs--focused' : '',
+              expiry.substr(0, 1) !== '•' ? 'rccs--filled' : '',
+            ]
+              .join(' ')
+              .trim()}
+          >
+            <div className="rccs__expiry__valid">{locale.valid}</div>
+            <div className="rccs__expiry__value">{expiry}</div>
+          </div>
+          <div className="rccs__chip" />
+        </div>
+        <div className="rccs__card--back">
+          <div className="rccs__card__background" />
+          <div className="rccs__stripe" />
+          <div className="rccs__signature" />
+          <div
+            className={['rccs__cvc', focused === 'cvc' ? 'rccs--focused' : '']
+              .join(' ')
+              .trim()}
+          >
+            {cvc}
+          </div>
+          <div className="rccs__issuer" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+*/
+/*
+
+*/
