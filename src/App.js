1dd2c7ce03330a31a9a7b1221db800bd94548781
mohamedisakr <mohamedisakr@gmail.com>
2022-03-01 19:27:52 +0200

    updating credit card controls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
+import valid from 'card-validator'
 // import Cleave from 'cleave.js/react'
 import 'animate.css'
 import './App.css'
@@ -9,14 +10,14 @@ import {toast} from 'react-toastify'
 // import ReactDOM from 'react-dom'
 // 'cleave.js/dist/cleave-react.min.js'
 
-const imageUrls = [
-  'https://logos-world.net/wp-content/uploads/2020/04/Visa-Logo.png',
-  'https://brand.mastercard.com/content/dam/mccom/brandcenter/thumbnails/mastercard_vrt_rev_92px_2x.png',
-  'https://www.discover.com/company/images/newsroom/media-downloads/discover.png',
-  'https://s1.q4cdn.com/692158879/files/design/svg/american-express-logo.svg',
-  'https://cdn4.iconfinder.com/data/icons/simple-peyment-methods/512/diners_club-512.png',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/JCB_logo.svg/1280px-JCB_logo.svg.png',
-]
+// const imageUrls = [
+//   'https://logos-world.net/wp-content/uploads/2020/04/Visa-Logo.png',
+//   'https://brand.mastercard.com/content/dam/mccom/brandcenter/thumbnails/mastercard_vrt_rev_92px_2x.png',
+//   'https://www.discover.com/company/images/newsroom/media-downloads/discover.png',
+//   'https://s1.q4cdn.com/692158879/files/design/svg/american-express-logo.svg',
+//   'https://cdn4.iconfinder.com/data/icons/simple-peyment-methods/512/diners_club-512.png',
+//   'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/JCB_logo.svg/1280px-JCB_logo.svg.png',
+// ]
 
 function App() {
   const [creditCard, setCreditCard] = useState('')
